53852efad6fc2940b533ed4a0b4608bf9e87d805
Luke Daley <ld@ldaley.com>
2010-11-18 11:22:30 +1000

    Javadocs.

--- src/groovy/grails/plugin/localevariant/LocaleVariantResolver.groovy
+++ src/groovy/grails/plugin/localevariant/LocaleVariantResolver.groovy
@@ -18,8 +18,21 @@ package grails.plugin.localevariant
 
 import javax.servlet.http.HttpServletRequest
 
+/**
+ * A locale variant resolver determines the locale variant to use for a particular request.
+ * 
+ * It is called whenever an explicit locale is set programatically. Or in the absence of a set
+ * locale, when resolving the locale based on the user request.
+ */
 interface LocaleVariantResolver {
 	
+	/**
+	 * For the given request, return the locale variant to use.
+	 * 
+	 * @param locale The already determined locale for the request, before adding the variant
+	 * @param request The request we are determining the locale variant for
+	 * @return the locale variant to use, or null if no variant should be used 
+	 */
 	String resolveLocaleVariant(Locale locale, HttpServletRequest request)
 	
 }
